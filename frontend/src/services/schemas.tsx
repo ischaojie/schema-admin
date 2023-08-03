0bacc1da5c9c34b208409ebe5c6da11999f526df
Chaojie <hi@chaojie.fun>
2023-08-03 16:58:15 +0800

    frontend: using replaced fetch to axios

--- frontend/src/services/schemas.tsx
+++ frontend/src/services/schemas.tsx
@@ -1,29 +1,30 @@
-
+import axios from "axios";
 
 // /api/models/<name>
 export async function getSchema(name: string) {
-  return await fetch(`/admin/api/schemas/${name}`, {
-    method: "GET",
-  })
-    .then((rsp) => rsp.json())
+  return await axios
+    .get(`/admin/api/schemas/${name}`)
+    .then((rsp) => rsp.data)
     .catch((err) => console.error(err));
 }
 
 // /api
 export async function getMetadata() {
-  return await fetch("/admin/api", { method: "GET" })
-    .then((rsp) => rsp.json())
+  return await axios
+    .get("/admin/api")
+    .then((rsp) => rsp.data)
     .catch((err) => console.error(err));
 }
 
 export async function saveSchemaData(name: string, data: any) {
-  return await fetch(`/admin/api/schemas/${name}/data`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((rsp) => rsp.json())
+  return await axios
+    .post(`/admin/api/schemas/${name}/data`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    .then((rsp) => rsp.data)
     .catch((err) => console.error(err));
 }
